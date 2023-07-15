d490e6e74bacdb591d0d06a2fae19c906f6ef438
newkayak12 <newkayak12@gmail.com>
2023-07-16 01:25:00 +0900

    sign

---
+++ netflix_clone/component/sign/step2/PasswordInput.swift
@@ -0,0 +1,107 @@
+//
+//  passwordInput.swift
+//  netflix_clone
+//
+//  Created by Sang Hyeon kim on 2023/07/15.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+import RxSwift
+import SnapKit
+import RxCocoa
+
+
+final class PasswordInput: UIView {
+    public var value: String = ""
+    public var isSecureRaw = true
+    public var isSecure: BehaviorSubject<Bool> = BehaviorSubject<Bool>(value: true)
+    
+    
+    private lazy var title = {
+        let label = UILabel()
+        label.text = "비밀번호를 입력해주세요"
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        label.textColor = .white
+        label.snp.makeConstraints { make in
+            make.height.equalTo(26)
+        }
+        return label;
+    }()
+    private lazy var xButton = {
+        let button = UIButton()
+        let xImage = UIImage(systemName: "eye")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        button.setImage(xImage, for: .normal)
+        button.addTarget(self, action: #selector(self.clear), for: .touchUpInside)
+        return button
+    }()
+    private lazy var input = {
+        let field = UITextField()
+        field.borderStyle = .roundedRect
+        field.textColor = .white
+        field.backgroundColor = .black
+        field.tintColor = .white
+        field.placeholder = "비밀번호를 입력해주세요."
+        field.rightViewMode = .always
+        field.rightView = self.xButton
+        field.font = UIFont.systemFont(ofSize: 20)
+        
+        self.isSecure
+            .withUnretained(self)
+            .bind { this, value in
+                field.isSecureTextEntry = value
+                switch value {
+                    case true:
+                        let xImage = UIImage(systemName: "eye")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+                        this.xButton.setImage(xImage, for: .normal)
+                        this.isSecureRaw = value
+                    case false:
+                        let xImage = UIImage(systemName: "eye.slash")?.withTintColor(.white, renderingMode: .alwaysOriginal)
+                        this.xButton.setImage(xImage, for: .normal)
+                        this.isSecureRaw = value
+                }
+            }
+            .disposed(by: rx.disposeBag)
+        
+        field.snp.makeConstraints { make in
+            make.height.equalTo(20)
+        }
+        return field
+    }()
+    private lazy var container = {
+        let stackView = UIStackView(arrangedSubviews: [self.title, self.input])
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.drawUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError ("init (coder:) has not been implemented" )
+    }
+    
+    
+    func drawUI () {
+        self.addSubview(self.container)
+        self.container.snp.makeConstraints { make in
+            make.width.height.leading.top.trailing.bottom.equalTo(self)
+        }
+    }
+    
+    func addInputEvent() -> ControlProperty<String?> {
+        return self.input.rx.text
+    }
+    
+    @objc
+    func clear () {
+        self.isSecureRaw = !self.isSecureRaw
+        self.isSecure.onNext(self.isSecureRaw)
+    }
+}
--- netflix_clone/view/sign/in/Step2ViewController.swift
+++ netflix_clone/view/sign/in/Step2ViewController.swift
@@ -8,15 +8,26 @@
 import Foundation
 import UIKit
 import SwiftUI
+import RxSwift
 
 class Step2ViewController: BaseViewController, ViewModelBindable {
+    lazy var passwordInput: PasswordInput = {
+        let input = PasswordInput(frame: .zero)
+        return input
+    }()
+    lazy var findPassword: UIButton = {
+        let findPwdButton = UIButton(frame: .zero)
+        findPwdButton.setTitle("비밀번호를 잃어버리셨나요?", for: .normal)
+        findPwdButton.tintColor = .lightGray
+        findPwdButton.addTarget(self, action: #selector(self.findPwd), for: .touchUpInside)
+        return findPwdButton
+    }()
+    
     var viewModel: Step2ViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        
-        Log.debug("ID", viewModel.id)
     }
     
     func setNavigation() {
@@ -25,13 +36,90 @@ class Step2ViewController: BaseViewController, ViewModelBindable {
         appearance.backgroundColor = UIColor.black
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        
+        let backButton = UIButton(frame: .zero)
+        backButton.addTarget(self, action: #selector(fnBackBtn), for: .touchUpInside)
+        backButton.tintColor = .white
+        backButton.setImage(UIImage(systemName: "chevron.backward", withConfiguration: UIImage.SymbolConfiguration(pointSize: CGFloat(18), weight: .bold)), for: .normal)
+        navigationItem.leftBarButtonItems = [UIBarButtonItem(customView: backButton)]
+        
+        let nextButton = UIButton(frame: .zero)
+        nextButton.setTitle("다음", for: .normal)
+        nextButton.addTarget(self, action: #selector(fnNextBtn), for: .touchUpInside)
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: nextButton)]
+    }
+    
+    func setConstraints () {
+        
+        passwordInput.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.bottom.equalTo(view.snp.centerY)
+            make.width.equalTo(view)
+        }
+        
+        findPassword.snp.makeConstraints { make in
+            make.leading.equalTo(view).offset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(onKeyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(onKeyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
     func wireViewModel() {
+        passwordInput.addInputEvent()
+            .withUnretained(self)
+            .bind{
+                if let text = $1 {
+                    $0.viewModel.password = text
+                }
+            }.disposed(by: rx.disposeBag)
         
     }
     
     func prepareUI() {
+        view.addSubview(passwordInput)
+        view.addSubview(findPassword)
         
+        
+        self.setConstraints()
+    }
+    
+    @objc
+    func fnBackBtn() {
+        navigationController?.popViewController(animated: true)
+    }
+    @objc
+    func fnNextBtn() {
+    }
+    
+    @objc
+    func findPwd() {
+        
+    }
+    
+    @objc
+    func onKeyboardUp (_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            findPassword.snp.removeConstraints()
+            findPassword.snp.makeConstraints { make in
+                make.leading.equalTo(view).offset(20)
+                make.bottom.equalTo(view).offset(-keyboardHeight)
+            }
+        }
+        
+    }
+    @objc
+    func onKeyboardDown (_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            findPassword.snp.removeConstraints()
+            findPassword.snp.makeConstraints { make in
+                make.leading.equalTo(view).offset(20)
+                make.bottom.equalTo(view.layoutMarginsGuide)
+            }
+        }
     }
 }
